e27a7234b5c9ceabf13ea599f045418410490c91
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-03-12 10:42:57 +0530

    Time: 29 ms (71.78%) | Memory: 31.6 MB (54.92%) - LeetSync

--- 932-monotonic-array/monotonic-array.py
+++ 932-monotonic-array/monotonic-array.py
@@ -10,4 +10,5 @@ class Solution:
         #         decreasing = False
         
         # return increasing or decreasing
-        return nums == sorted(nums) or nums == sorted(nums, reverse = True)
+        #return nums == sorted(nums) or nums == sorted(nums, reverse = True)
+        return all(nums[i] <= nums[i+1] for i in range(len(nums)-1)) or all(nums[i] >= nums[i+1] for i in range(len(nums)-1)) 
