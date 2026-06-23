a049b247ff3b1cfc1fb2302cc19784c343da9d62
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-06-23 14:58:14 +0530

    Time: 7 ms (43.31%) | Memory: 19.6 MB (52.18%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,6 +1,7 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         people = list(zip(heights, names))
+        
         people.sort(reverse = True)
         res = []
         for heights, name in people:
